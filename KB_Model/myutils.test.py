a33e47b6ec7fbd342cb7c46929ba680920695679
Sten Sipma <sten.sipma@ziggo.nl>
2024-03-11 09:35:28 +0100

    Test using unittest lib

--- KB_Model/myutils.test.py
+++ KB_Model/myutils.test.py
@@ -1,93 +1,90 @@
+import unittest
+
 import myutils
 import numba
 import numpy as np
 
 
-def test_find_root_secant():
-    @numba.jit(nopython=True)
-    def test_func(x):
-        return x - 1
+class TestUtil(unittest.TestCase):
+    def test_find_root_secant(self):
+        @numba.jit(nopython=True)
+        def test_func(x, _args):
+            return x - 1
 
-    found_x = myutils.find_root_secant(test_func, 10, -10, tol=1e-6)
-    assert abs(found_x - 1) <= 1e-6
+        found_x = myutils.find_root_secant(test_func, 10, -10, tol=1e-6)
+        self.assertTrue(abs(found_x - 1) <= 1e-6)
 
-    @numba.jit(nopython=True)
-    def test_func_2(x):
-        return x + 1
+        @numba.jit(nopython=True)
+        def test_func_2(x, _args):
+            return x + 1
 
-    found_x = myutils.find_root_secant(test_func_2, 2e-3 - 1, -1e-3 - 1, tol=1e-6)
-    assert abs(found_x + 1) <= 1e-6
+        found_x = myutils.find_root_secant(test_func_2, 2e-3 - 1, -1e-3 - 1, tol=1e-6)
+        self.assertTrue(abs(found_x + 1) <= 1e-6)
 
+    def test_find_segment(self):
+        test_list = np.arange(10)
 
-def test_find_segment():
-    test_list = np.arange(10)
+        self.assertEqual(myutils.find_segment(5.5, test_list), 5)
+        self.assertEqual(myutils.find_segment(0, test_list), 0)
+        self.assertEqual(myutils.find_segment(5, test_list), 5)
 
-    assert myutils.find_segment(5.5, test_list) == 5
-    assert myutils.find_segment(0, test_list) == 0
-    assert myutils.find_segment(5, test_list) == 5
+        # TODO: fix this test case ()
+        # assert myutils.find_segment(9, test_list) == 8
 
-    # TODO: fix this test case ()
-    # assert myutils.find_segment(9, test_list) == 8
+        # TODO: implement extrapolation (check if it works with interp.)
+        self.assertEqual(myutils.find_segment(-10, test_list), 0)
+        self.assertEqual(myutils.find_segment(20, test_list), 8)
 
-    # TODO: implement extrapolation (check if it works with interp.)
-    assert myutils.find_segment(-10, test_list) == 0
-    assert myutils.find_segment(20, test_list) == 8
+    def test_linear_spline(self):
+        x_test_list = np.arange(10)
+        y_test_list = x_test_list + 1
 
+        self.assertEqual(myutils.linear_spline(0, x_test_list, y_test_list), 1)
+        self.assertEqual(myutils.linear_spline(7, x_test_list, y_test_list), 8)
+        self.assertEqual(myutils.linear_spline(3.1234, x_test_list, y_test_list), 4.1234)
+        self.assertEqual(myutils.linear_spline(9, x_test_list, y_test_list), 10)
 
-def test_linear_spline():
-    x_test_list = np.arange(10)
-    y_test_list = x_test_list + 1
+        # Test extrapolation ?
+        self.assertEqual(myutils.linear_spline(20, x_test_list, y_test_list), 21)
+        self.assertEqual(myutils.linear_spline(-30, x_test_list, y_test_list), -29)
 
-    assert myutils.linear_spline(0, x_test_list, y_test_list) == 1
-    assert myutils.linear_spline(7, x_test_list, y_test_list) == 8
-    assert myutils.linear_spline(3.1234, x_test_list, y_test_list) == 4.1234
-    assert myutils.linear_spline(9, x_test_list, y_test_list) == 10
+        # TODO: add more test cases for nonlinear functions
 
-    # Test extrapolation ?
-    assert myutils.linear_spline(20, x_test_list, y_test_list) == 21
-    assert myutils.linear_spline(-30, x_test_list, y_test_list) == -29
+    def test_bisect(self):
+        @numba.jit(nopython=True)
+        def test_func(x):
+            return x - 1
 
-    # TODO: add more test cases for nonlinear functions
+        found_x = myutils.bisect(test_func, 10, -10, x_tol=1e-10)
+        self.assertTrue(abs(found_x - 1) <= 1e-10)
 
+        # Works both ways:
+        found_x = myutils.bisect(test_func, -10, 10, x_tol=1e-10)
+        self.assertTrue(abs(found_x - 1) <= 1e-10)
 
-def test_bisect():
-    @numba.jit(nopython=True)
-    def test_func(x):
-        return x - 1
+        @numba.jit(nopython=True)
+        def test_func_2(x):
+            return x
 
-    found_x = myutils.bisect(test_func, 10, -10, x_tol=1e-10)
-    assert abs(found_x - 1) <= 1e-10
+        found_x = myutils.bisect(test_func_2, 1e-10, -1e-10, x_tol=1e-20)
+        self.assertTrue(abs(found_x) <= 1e-20)
 
-    # Works both ways:
-    found_x = myutils.bisect(test_func, -10, 10, x_tol=1e-10)
-    assert abs(found_x - 1) <= 1e-10
+        try:
+            found_x = myutils.bisect(test_func, -10, -8, x_tol=1e-10)
+            self.fail("This call must raise a ValueError")
+        except ValueError:
+            pass
+        except Exception:
+            self.fail("Call raised the wrong exception")
 
-    @numba.jit(nopython=True)
-    def test_func_2(x):
-        return x
-
-    found_x = myutils.bisect(test_func_2, 1e-10, -1e-10, x_tol=1e-20)
-    assert abs(found_x) <= 1e-20
-
-    try:
-        found_x = myutils.bisect(test_func, -10, -8, x_tol=1e-10)
-        assert False, "This call must raise a ValueError"
-    except ValueError:
-        pass
-    except Exception:
-        assert False, "Call raised the wrong exception"
-
-    try:
-        found_x = myutils.bisect(test_func, -10, 10, x_tol=1e-50, max_iter=5)
-        assert False, "This call must raise a RuntimeError"
-    except RuntimeError:
-        pass
-    except Exception:
-        assert False, "Call raised the wrong exception"
+        try:
+            found_x = myutils.bisect(test_func, -10, 10, x_tol=1e-50, max_iter=5)
+            self.fail("This call must raise a RuntimeError")
+        except RuntimeError:
+            pass
+        except Exception:
+            self.fail("Call raised the wrong exception")
 
 
 if __name__ == "__main__":
-    test_find_segment()
-    test_linear_spline()
-    test_bisect()
-    test_find_root_secant()
+    unittest.main()
