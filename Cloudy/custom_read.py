e58a5b1c9284a46ee3a111934a37be9adc0c8a25
S.K. Sipma <sipma@virgo07.intra.astro.rug.nl>
2024-03-25 12:35:31 +0100

    Fix CLOUDY interpolator

--- Cloudy/custom_read.py
+++ Cloudy/custom_read.py
@@ -3,43 +3,6 @@ from pathlib import Path
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 
-table_location = Path("./FinalTable.txt")
-data = np.loadtxt(table_location, comments="#")
-
-headers = ["Hden", "Metals", "T", "Htot", "Ctot", "Molwgt", "dens"]
-
-# The dimensions of each axis
-dim_T = 101
-dim_Z = 6
-dim_n_H = 7
-shape = (dim_n_H, dim_Z, dim_T)
-
-# All the data as a cube
-n_H = data[:, 0].reshape(shape)
-Z = data[:, 1].reshape(shape)
-T = data[:, 2].reshape(shape)
-heating = data[:, 3].reshape(shape)
-cooling = data[:, 4].reshape(shape)
-mol_weight = data[:, 5].reshape(shape)
-rho = data[:, 6].reshape(shape)
-
-# The axes:
-n_H_ax = n_H[:, 0, 0]
-Z_ax = Z[0, :, 0]
-T_ax = T[0, 0, :]
-
-axes = (n_H_ax, Z_ax, T_ax)
-
-heating_interp = RegularGridInterpolator(axes, heating)
-cooling_interp = RegularGridInterpolator(axes, cooling)
-rho_interp = RegularGridInterpolator(axes, rho)
-mol_weight_interp = RegularGridInterpolator(axes, mol_weight)
-
-
-# print(heating_interp(test_basic), heating[i, j, k])
-# print(cooling_interp(test_basic), cooling[i, j, k])
-# print(mol_weight_interp(test_basic), mol_weight[i, j, k])
-
 
 class CloudyCoolingFunc:
     def __init__(self, data_location="./FinalTable.txt"):
@@ -48,6 +11,9 @@ class CloudyCoolingFunc:
         # Unpack
         self.heating_int, self.cooling_int, self.rho_int, self.mol_weight_int = interp_funcs
         self.n_H, self.Z, self.T = axes
+
+    def __str__(self):
+        return f"CloudyCoolingFunc(limits: n_H={self.n_H[0]} to {self.n_H[-1]}, Z={self.Z[0]} to {self.Z[-1]}, T={self.T[0]} to {self.T[-1]})"
 
     def __call__(self, n_H, Z, T):
         """
@@ -78,7 +44,7 @@ class CloudyCoolingFunc:
     # Helper functions for loading
     @staticmethod
     def _interp_3d_helper(interp, n_H, Z, T):
-        test_points = np.array([n_H, Z, T])
+        test_points = np.vstack([n_H, Z, T]).T
         return interp(test_points)
 
     @staticmethod
@@ -115,16 +81,54 @@ class CloudyCoolingFunc:
 
         return (heating_interp, cooling_interp, rho_interp, mol_weight_interp), (n_H_ax, Z_ax, T_ax)
 
+if __name__ == '__main__':
+    table_location = Path("./FinalTable.txt")
+    data = np.loadtxt(table_location, comments="#")
 
-# TEST:
-i = 5
-j = 1
-k = 0
-test_basic = [n_H_ax[i], Z_ax[j], T_ax[k]]
+    headers = ["Hden", "Metals", "T", "Htot", "Ctot", "Molwgt", "dens"]
 
-cool = CloudyCoolingFunc()
+    # The dimensions of each axis
+    dim_T = 101
+    dim_Z = 6
+    dim_n_H = 7
+    shape = (dim_n_H, dim_Z, dim_T)
 
-print(cool.heating(*test_basic), heating[i, j, k])
-print(cool.cooling(*test_basic), cooling[i, j, k])
-print(cool.mol_weight(*test_basic), mol_weight[i, j, k])
-print(cool.rho(*test_basic), rho[i, j, k])
+    # All the data as a cube
+    n_H = data[:, 0].reshape(shape)
+    Z = data[:, 1].reshape(shape)
+    T = data[:, 2].reshape(shape)
+    heating = data[:, 3].reshape(shape)
+    cooling = data[:, 4].reshape(shape)
+    mol_weight = data[:, 5].reshape(shape)
+    rho = data[:, 6].reshape(shape)
+
+    # The axes:
+    n_H_ax = n_H[:, 0, 0]
+    Z_ax = Z[0, :, 0]
+    T_ax = T[0, 0, :]
+
+    axes = (n_H_ax, Z_ax, T_ax)
+
+    heating_interp = RegularGridInterpolator(axes, heating)
+    cooling_interp = RegularGridInterpolator(axes, cooling)
+    rho_interp = RegularGridInterpolator(axes, rho)
+    mol_weight_interp = RegularGridInterpolator(axes, mol_weight)
+
+
+    # print(heating_interp(test_basic), heating[i, j, k])
+    # print(cooling_interp(test_basic), cooling[i, j, k])
+    # print(mol_weight_interp(test_basic), mol_weight[i, j, k])
+
+
+    # TEST:
+    i = 5
+    j = 1
+    k = 0
+    test_basic = [n_H_ax[i], Z_ax[j], T_ax[k]]
+
+    cool = CloudyCoolingFunc()
+
+    print(cool.heating(*test_basic), heating[i, j, k])
+    print(cool.cooling(*test_basic), cooling[i, j, k])
+    print(cool.mol_weight(*test_basic), mol_weight[i, j, k])
+    print(cool.rho(*test_basic), rho[i, j, k])
